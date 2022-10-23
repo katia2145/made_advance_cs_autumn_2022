6fec41cde12f8a0437a0bce7873fa76008721412
katia2145 <katya.potapova88@gmail.com>
2022-10-23 16:27:52 +0300

    csv reader added

---
+++ csv_reader.py
@@ -0,0 +1,6 @@
+import csv
+
+with open('2.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
